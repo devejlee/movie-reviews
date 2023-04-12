44f5bf701f800014c341468c33d11971f8810b13
Elijah Lee <develijahlee@gmail.com>
2023-04-12 16:03:59 +0900

    add TextInput to ReviewSearchWrap

--- src/components/ReviewSearchWrap.tsx
+++ src/components/ReviewSearchWrap.tsx
@@ -1,7 +1,10 @@
+import { TextInput } from './TextInput'
+
 const ReviewSearchWrap = () => {
   return (
     <div className='wrap reviewSearchWrap'>
       <h2>리뷰 검색</h2>
+      <TextInput placeholderText='영화 제목을 입력해 주세요' />
     </div>
   )
 }
