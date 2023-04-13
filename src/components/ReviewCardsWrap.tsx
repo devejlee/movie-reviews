9ce43d248a387d1ba669614b975ae8927c4b1ffd
Elijah Lee <develijahlee@gmail.com>
2023-04-13 17:46:42 +0900

    add sortMovies

--- src/components/ReviewCardsWrap.tsx
+++ src/components/ReviewCardsWrap.tsx
@@ -1,11 +1,14 @@
 import { ReviewCard } from './ReviewCard'
 import { initialData } from '../data'
+import { sortMovies } from '../utils';
+
+const sortedData = sortMovies(initialData)
 
 const ReviewCardsWrap = () => {
   return (
     <div className='wrap reviewCardsWrap'>
       <h2>리뷰 내역</h2>
-      {initialData.map(movie => {
+      {sortedData.map(movie => {
         return (
           <ReviewCard key={movie.id} title={movie.title} comment={movie.comment} score={movie.score} />
         )
---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+type Movie = {
+  id: number
+  title: string
+  comment: string
+  score: number
+}
+
+const patternAlphabet = /[a-zA-Z]/
+const patternHangul = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/
+const orderLevelDesc = [patternHangul, patternAlphabet]
+
+const getPatternIndex = (s: string) => {
+  const index = orderLevelDesc.findIndex((pattern) => pattern.test(s))
+  return index;
+}
+
+export const sortMovies = (source: Movie[]) => {
+  const sortedData = [...source].sort((a, b) => {
+    const aLevel = getPatternIndex(a.title.charAt(0))
+    const bLevel = getPatternIndex(b.title.charAt(0))
+    if (aLevel === bLevel) {
+      return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+    }
+    return bLevel - aLevel;
+  })
+
+
+  return sortedData;
+}
