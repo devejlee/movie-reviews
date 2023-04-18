3c31e69ff9f77f33374c5c971508edc52b06d9fa
Elijah Lee <develijahlee@gmail.com>
2023-04-18 22:47:18 +0900

    keep old ReviewInputWrap colors

--- src/components/ReviewInputWrap/styles.ts
+++ src/components/ReviewInputWrap/styles.ts
@@ -3,21 +3,19 @@ import { baseStyle } from '../../baseStyles'
 
 export const Wrap = styled.div`
   ${baseStyle}
-  background: #ffb9ac;
+  background: #f5f5f5;
 `
 
 export const Button = styled.button`
   width: 100%;
   height: 40px;
   margin-top: 20px;
-  background: #ff7f50;
+  background: #194e84;
   border-radius: 10px;
   color: #fff;
   font-size: 15px;
   border: none;
-  transition: background-color 0.3s ease;
   &:hover {
     cursor: pointer;
-    background: #ff6347; /* Updated background color on hover */
   }
 `;
