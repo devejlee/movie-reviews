417bcd66abb8db4862fa3804905887d636ff6075
Elijah Lee <develijahlee@gmail.com>
2023-05-03 21:23:17 +0900

    create videoPlayer route

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home'
 import Todo from './pages/Todo'
+import VideoPlayer from './pages/VideoPlayer'
 import './index.scss'
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 
@@ -12,11 +13,13 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/todo">Todo</Link></li>
+          <li><Link to="/videoPlayer">VideoPlayer</Link></li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/videoPlayer" element={<VideoPlayer />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
